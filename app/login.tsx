a7017a161212bf163517b02cb3f8f84ee6361c47
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2025-02-25 15:27:33 -0600

    Updated Login and Registration Screens

--- app/login.tsx
+++ app/login.tsx
@@ -1,22 +1,84 @@
-import { Link, useRouter } from 'expo-router';
-import { Pressable, Text, View } from 'react-native';
+import { useRouter } from 'expo-router';
+import { View, Text, TextInput, Pressable, StyleSheet, Image } from 'react-native';
 
-export default function Page() {
+export default function LoginScreen() {
     const router = useRouter();
-    return (
-        <View style= {{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Login</Text>
-            <Link href="/register" replace>
-                <Text>Create a New Account</Text>
-            </Link>
 
-            <Pressable
-                onPress={() => {
-                    router.replace('/(tabs)');
-                }}
-            >
-                <Text>Sign In</Text>
-                </Pressable>
+    return (
+        <View style={styles.container}>
+            <Image source={require('@/assets/images/logo.png')} style={styles.logo} resizeMode='contain'/>
+
+            <Text style={styles.loginText}>Login</Text>
+
+            <TextInput style={styles.input} placeholder="Email" placeholderTextColor="#999" />
+            <TextInput style={styles.input} placeholder="Password" placeholderTextColor="#999" secureTextEntry />
+
+            <Pressable style={styles.signInButton} onPress={() => router.replace('/(tabs)')}>
+                <Text style={styles.signInText}>Sign in</Text>
+            </Pressable>
+
+
+            <Pressable style={styles.registerButton} onPress={() => router.replace('/register')}>
+                <Text style={styles.registerText}>Create a new account</Text>
+            </Pressable>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#03053D',
+        paddingHorizontal: 20,
+    },
+    logo: { 
+        width: '100%',
+        height: 120,
+        marginBottom: 30
+    },
+    loginText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#fff',
+        marginBottom: 20,
+    },
+    input: {
+        width: '100%',
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#00C896',
+        borderRadius: 5,
+        paddingHorizontal: 15,
+        backgroundColor: '#03053D',
+        color: '#fff',
+        marginBottom: 15,
+    },
+    signInButton: {
+        width: '100%',
+        height: 50,
+        backgroundColor: '#00C896',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+        marginBottom: 10,
+        marginTop: 20,
+    },
+    signInText: {
+        color: '#03053D',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    registerButton: {
+        width: '100%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+    },
+    registerText: {
+        color: '#fff',
+        fontSize: 16,
+    },
+});
--- app/register.tsx
+++ app/register.tsx
@@ -1,13 +1,87 @@
-import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import { useRouter } from 'expo-router';
+import { View, Text, TextInput, Pressable, StyleSheet, Image } from 'react-native';
 
-export default function Page() {
+export default function RegisterScreen() {
+    const router = useRouter();
+
     return (
-        <View style= {{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Register</Text>
-            <Link href="/login" replace>
-                <Text>Log into Existing Account</Text>
-            </Link>
+        <View style={styles.container}>
+             <Image source={require('../assets/images/logo.png')} style={styles.logo} resizeMode='contain' />
+
+            <Text style={styles.registerText}>Register</Text>
+
+            <TextInput style={styles.input} placeholder="Email" placeholderTextColor="#999" />
+            <TextInput style={styles.input} placeholder="Password" placeholderTextColor="#999" secureTextEntry />
+
+            <Pressable style={styles.createAccountButton} onPress={() => router.replace('/(tabs)')}>
+                <Text style={styles.createAccountText}>Create Account</Text>
+            </Pressable>
+
+            <Pressable style={styles.loginButton} onPress={() => router.replace('/login')}>
+                <Text style={styles.loginText}>Login to existing account</Text>
+            </Pressable>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#03053D',
+        paddingHorizontal: 20,
+    },
+    logo: { 
+        width: '100%',
+        height: 120,
+        marginBottom: 30
+    },
+    school: {
+        color: '#00C896',
+    },
+    registerText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#fff',
+        marginBottom: 20,
+    },
+    input: {
+        width: '100%',
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#00C896',
+        borderRadius: 5,
+        paddingHorizontal: 15,
+        backgroundColor: '#03053D',
+        color: '#fff',
+        marginBottom: 15,
+    },
+    createAccountButton: {
+        width: '100%',
+        height: 50,
+        backgroundColor: '#00C896',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+        marginTop: 20,
+        marginBottom: 10,
+    },
+    createAccountText: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    loginButton: {
+        width: '100%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+    },
+    loginText: {
+        color: '#fff',
+        fontSize: 16,
+        borderWidth: 0,
+    },
+});
