2da6076ebc440dbfbe96764eef1f57e6daa33f4a
Benjamin Carter <38075349+bcart01v@users.noreply.github.com>
2025-03-04 18:58:14 -0600

    Forgt the logout componenet

--- components/LogoutComponent.tsx
+++ components/LogoutComponent.tsx
@@ -1,11 +1,14 @@
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import { Pressable } from "react-native";
+import { useAuth } from "./AuthProvider";
 
 export function LogoutComponent() {
     const router = useRouter();
+    const auth = useAuth();
 
-    function lougout() {
+    async function lougout() {
+        await auth.logout();
         router.replace('/login');
     }
 
